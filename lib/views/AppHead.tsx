160eb9bd4e985952acd039506fc649b367ed191b
davidedelhart <dave@wonderlandlabs.com>
2022-10-07 13:18:24 -0700

    updates to layout

---
+++ lib/views/AppHead.tsx
@@ -0,0 +1,9 @@
+import { Header, Heading } from "grommet";
+
+ const AppHead = () => (
+  <Header background="brand" pad={{vertical: '0å', horizontal: 'medium'}}>
+    <Heading level={1}>Site Head</Heading>
+  </Header>
+)
+
+export default AppHead;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { Grommet, Box, grommet as grommetTheme } from 'grommet'
+import styles from '../styles/App.module.css'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import AppHead from "../lib/views/AppHead";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <Grommet theme={grommetTheme}>
+      <Box height="100%" width="100%" direction="column" justify="start" overflow="hidden">
+        <AppHead/>
+        <Box overflow="auto">
+          <Component {...pageProps} />
+        </Box>
+      </Box>
+    </Grommet>
+  )
 }
-
-export default MyApp
